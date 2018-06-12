52207e56dbc4d34a5c558164ad2ce530710445a2
Dea-k <lee5250@fredonia.edu>
2018-06-12 12:07:11 -0400

    .

--- ReplicateVenmo/app/src/main/java/com/example/deakyu/replicatevenmo/help/faq/CategoryListAdapter.java
+++ ReplicateVenmo/app/src/main/java/com/example/deakyu/replicatevenmo/help/faq/CategoryListAdapter.java
@@ -7,6 +7,8 @@ import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 import android.widget.TextView;
 
 import com.example.deakyu.replicatevenmo.R;
@@ -18,20 +20,28 @@ public class CategoryListAdapter extends RecyclerView.Adapter<CategoryListAdapte
     public class ViewHolder extends RecyclerView.ViewHolder {
         public ConstraintLayout row;
         public TextView category;
+        public ListView topicList;
 
         public ViewHolder(View iv) {
             super(iv);
 
             row = iv.findViewById(R.id.category_row);
             category = iv.findViewById(R.id.category_text);
+            topicList = iv.findViewById(R.id.topic_list_view);
         }
     }
 
     private final LayoutInflater inflater;
     private List<Category> categories;
+    private Context context;
+
+    private Context getContext() {
+        return context;
+    }
 
     public CategoryListAdapter(Context context) {
         this.inflater = LayoutInflater.from(context);
+        this.context = context;
     }
 
     @NonNull
@@ -45,7 +55,12 @@ public class CategoryListAdapter extends RecyclerView.Adapter<CategoryListAdapte
     public void onBindViewHolder(@NonNull ViewHolder vh, int pos) {
         Category currentCategory = categories.get(pos);
 
+        String[] topics = new String[currentCategory.getTopics().size()];
+        for(int i=0 ; i < topics.length ; i++) topics[i] = currentCategory.getTopics().get(i).getTopic();
+
+        ArrayAdapter<String> listAdapter = new ArrayAdapter<String>(getContext(), R.layout.topic_row,topics);
         vh.category.setText(currentCategory.getCategory());
+        vh.topicList.setAdapter(listAdapter);
     }
 
     public void setCategories(List<Category> categories) {
