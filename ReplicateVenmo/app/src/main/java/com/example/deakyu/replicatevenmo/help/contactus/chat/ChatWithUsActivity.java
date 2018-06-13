84cbc1f859681339e29879a68c8dbe37c1b33ac5
Dea-k <lee5250@fredonia.edu>
2018-06-12 21:51:18 -0400

    ChatWithUsActivity boilerplate done

--- ReplicateVenmo/app/src/main/java/com/example/deakyu/replicatevenmo/help/contactus/chat/ChatWithUsActivity.java
+++ ReplicateVenmo/app/src/main/java/com/example/deakyu/replicatevenmo/help/contactus/chat/ChatWithUsActivity.java
@@ -1,15 +1,77 @@
 package com.example.deakyu.replicatevenmo.help.contactus.chat;
 
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
 
 import com.example.deakyu.replicatevenmo.R;
 
-public class ChatWithUsActivity extends AppCompatActivity {
+public class ChatWithUsActivity extends AppCompatActivity implements AdapterView.OnItemSelectedListener {
+
+    private Toolbar toolbar;
+    private Spinner issueSpinner;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_chat_with_us);
+
+        initToolbar();
+        initSpinner();
+    }
+
+    private void initToolbar() {
+        toolbar = findViewById(R.id.chat_with_us_toolbar);
+        setSupportActionBar(toolbar);
+        ActionBar ab = getSupportActionBar();
+        ab.setDisplayHomeAsUpEnabled(true);
+    }
+
+    private void initSpinner() {
+        issueSpinner = findViewById(R.id.issue_spinner);
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(
+                this, R.array.issues_array, android.R.layout.simple_spinner_item
+        );
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        issueSpinner.setAdapter(adapter);
+        issueSpinner.setOnItemSelectedListener(this);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.chat, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+
+        if(id == R.id.submit) {
+            // Post request to server - create new chat
+            System.out.println("DEE submit clicked");
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+        // An item was selected. You can retrieve the selected item using
+        // parent.getItemAtPosition(pos)
+        System.out.println("DEE " + parent.getItemAtPosition(position).toString());
+    }
+
+    @Override
+    public void onNothingSelected(AdapterView<?> parent) {
+        // Another interface callback
+        System.out.println("DEE nothing selected");
     }
 }
